51018b7074448fab797eaf37836a3791ccb4b384
arzzzzi <kantyshevmagomed@gmail.com>
2021-12-29 15:57:02 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-
+class ItemList {
+    constructor() {
+        this.container = document.querySelector('.main-container');
+        this.ItemList = [];
+    }
+    addItem() {
+        const firstItem = this.container.firstChild;
+        const item = new Item(
+            item => this.deleteItem(item)
+        );
+        this.ItemList.push(item);
+        this.container.insertBefore(item.container, firstItem);
+    }
+    deleteItem(item) {
+        const id = this.itemList.indexOf(item);
+        this.itemList.splice(id, 1);
+        item.container.remove();
+    }
+}
